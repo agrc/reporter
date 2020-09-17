2fc35d337d57f556c9cf54168d4482257cb49286
Jake Adams <jdadams@utah.gov>
2020-09-17 14:34:40 -0600

    fix: cleaning cruft

--- tests/test_reports.py
+++ tests/test_reports.py
@@ -4,8 +4,6 @@ from reporter import reports
 def test_AGOL_save_report_gets_columns_right(mocker, tmp_path):
     test_data = [{'foo': 1, 'bar': 2}, {'bar': 4, 'foo': 3}]
     out_path = tmp_path / 'test.csv'
-    # mocker.patch('reporter.reports.AGOLUsageReport')
-    # mocker.patch.object('reporter.reports.AGOLUsageReport', 'out_path', out_path)
 
     mock_datetime = mocker.patch('datetime.datetime')
     mock_datetime.now.return_value = 'foo_date'
@@ -18,4 +16,4 @@ def test_AGOL_save_report_gets_columns_right(mocker, tmp_path):
     content = out_path.read_text()
     assert content == 'foo_date,\nfoo,bar\n1,2\n3,4\n'
 
-    # def test_AGOL_create_report_itemid_not_in_metatable
+    # def test_AGOL_create_report_itemid_not_in_metatable()
