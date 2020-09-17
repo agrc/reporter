a453a0ccfcdba704e2d640d4b53ce3afc95e7807
Jake Adams <jdadams@utah.gov>
2020-09-17 14:20:23 -0600

    fix: add secrets template

---
+++ src/reporter/credentials_template.py
@@ -0,0 +1,13 @@
+"""
+Secrets template file. Ensure real secrets file is exlcluded from git in .gitignore
+"""
+
+ORG = ''
+USERNAME = ''
+PASSWORD = ''
+
+SGID_METATABLE = ''
+AGOL_METATABLE = ''
+
+HFS_CREDITS_PER_MB = 0.24
+DOLLARS_PER_CREDIT = 0.1
