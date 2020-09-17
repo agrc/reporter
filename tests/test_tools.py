9def9cf723fc68f99541deafd08d9a4cc3f643cd
Jake Adams <jdadams@utah.gov>
2020-09-17 09:29:18 -0600

    feat: update tests for sgid,open data checks

--- tests/test_tools.py
+++ tests/test_tools.py
@@ -3,6 +3,7 @@
 """
 
 import datetime
+from collections import namedtuple
 
 import pytest
 
@@ -16,7 +17,7 @@ def item(mocker):
     """
 
     group1_mock = mocker.Mock()
-    group1_mock.title = 'Utah SGID TestSGIDGroup'
+    group1_mock.title = 'TestSGIDGroup'
     group2_mock = mocker.Mock()
     group2_mock.title = 'test_group'
 
@@ -36,13 +37,28 @@ def item(mocker):
     return item
 
 
-def test_get_item_info_correct_values(mocker, item):
+@pytest.fixture(scope='function')
+def metatable_row(mocker):
+    """
+    Default set of valid SGID values for metatable.
+    """
+    metatable_row = mocker.Mock()
+    metatable_row.sgid_name = 'SGID.TEST.Layer'
+    metatable_row.agol_name = 'Utah Test Layer'
+    metatable_row.category = 'SGID'
+    metatable_row.authoritative = 'y'
+
+    return metatable_row
+
+
+def test_get_item_info_correct_values(mocker, item, metatable_row):
 
     org_mock = mocker.Mock()
 
     folder = 'folder'
+    open_data_groups = ['TestSGIDGroup']
 
-    test_dict = tools.Organization.get_item_info(org_mock, item, folder)
+    test_dict = tools.Organization.get_item_info(org_mock, item, open_data_groups, folder, metatable_row)
     assert test_dict['itemid'] == 'itemid'
     assert test_dict['title'] == 'title'
     assert test_dict['owner'] == 'owner'
@@ -50,8 +66,9 @@ def test_get_item_info_correct_values(mocker, item):
     assert test_dict['views'] == 42
     assert test_dict['modified'] == '2020-12-25 18:30:55'
     assert test_dict['authoritative'] == 'content_status'
-    assert test_dict['open_data'] == 'yes'
-    assert test_dict['groups'] == 'Utah SGID TestSGIDGroup, test_group'
+    assert test_dict['open_data_group'] == 'True'
+    assert test_dict['in_sgid'] == 'True'
+    assert test_dict['groups'] == 'TestSGIDGroup, test_group'
     assert test_dict['tags'] == 'tag1, tag2'
     assert test_dict['sizeMB'] == 12
     assert test_dict['credits'] == 12 * credentials.HFS_CREDITS_PER_MB
@@ -59,15 +76,39 @@ def test_get_item_info_correct_values(mocker, item):
     assert test_dict['data_requests_1Y'] == 1234
 
 
-def test_get_item_info_not_open_data(mocker, item):
+def test_get_item_info_not_open_data(mocker, item, metatable_row):
     org_mock = mocker.Mock()
 
     group_mock = mocker.Mock()
     group_mock.title = 'test_group'
-
-    # item = mocker.Mock()
     item.shared_with = {'groups': [group_mock]}
 
-    test_dict = tools.Organization.get_item_info(org_mock, item, 'folder')
+    open_data_groups = ['TestSGIDGroup']
 
-    assert test_dict['open_data'] == 'no'
+    test_dict = tools.Organization.get_item_info(org_mock, item, open_data_groups, 'folder', metatable_row)
+
+    assert test_dict['open_data_group'] == 'False'
+
+
+def test_get_item_info_shelved_data_not_in_sgid(mocker, item, metatable_row):
+    org_mock = mocker.Mock()
+
+    open_data_groups = ['TestSGIDGroup']
+
+    metatable_row.category = 'shelved'
+
+    test_dict = tools.Organization.get_item_info(org_mock, item, open_data_groups, 'folder', metatable_row)
+
+    assert test_dict['in_sgid'] == 'False'
+
+
+def test_get_item_info_static_data_in_sgid(mocker, item, metatable_row):
+    org_mock = mocker.Mock()
+
+    open_data_groups = ['TestSGIDGroup']
+
+    metatable_row.category = 'static'
+
+    test_dict = tools.Organization.get_item_info(org_mock, item, open_data_groups, 'folder', metatable_row)
+
+    assert test_dict['in_sgid'] == 'True'
