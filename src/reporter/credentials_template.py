e585af9792aca3f42e59ea5a9e30053e28c5de1e
Scott Davis <stdavis@utah.gov>
2020-09-29 08:13:23 -0600

    docs: add comments to help with filling out credentials
    Fixes #4

--- src/reporter/credentials_template.py
+++ src/reporter/credentials_template.py
@@ -2,14 +2,19 @@
 Secrets template file. Ensure real secrets file is excluded from git in .gitignore
 """
 
+#: A URL for your AGOL org. e.g. https://utah.maps.arcgis.com
 ORG = ''
+
 USERNAME = ''
 PASSWORD = ''
 
+#: The full path to the AGOLItems table in SGID
 SGID_METATABLE = ''
+#: The URL to the AGOLItems_shelved feature service layer
 AGOL_METATABLE = ''
 
 HFS_CREDITS_PER_MB = 0.24
 DOLLARS_PER_CREDIT = 0.1
 
+#: e.g. 'C:\\temp'
 REPORT_DIR = ''
