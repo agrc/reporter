645d8053817420a317cf1f84ecd64457d5f88656
Scott Davis <stdavis@utah.gov>
2020-09-29 08:34:52 -0600

    docs: more refinements

--- src/reporter/credentials_template.py
+++ src/reporter/credentials_template.py
@@ -8,13 +8,13 @@ ORG = ''
 USERNAME = ''
 PASSWORD = ''
 
-#: The full path to the AGOLItems table in SGID
+#: The full path to the AGOLItems table in SGID e.g. r'C:\temp\SGID.sde\TableName`
 SGID_METATABLE = ''
-#: The URL to the AGOLItems_shelved feature service layer
+#: The URL to the AGOLItems_shelved feature service layer e.g. https://server.com/arcgis/AGOLItems_shelved/FeatureServer/0
 AGOL_METATABLE = ''
 
 HFS_CREDITS_PER_MB = 0.24
 DOLLARS_PER_CREDIT = 0.1
 
-#: e.g. 'C:\\temp'
+#: e.g. r'C:\temp'
 REPORT_DIR = ''
